a624d174e4447382887e73fefcb2abd37ec02998
LIBERT Quentin <quentinlibert59@gmail.com>
2024-03-04 10:07:11 +0100

    date et heure

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,9 @@
 
 6
 app.py,1\3\13cce7fd076299c81b4986166f3d822791c9490e
+D
+templates/Index.html,1\3\13a0164f8f64255e51b669916567057f9ab31a10
+D
+templates/heure.html,0\1\01c54d79842ec9bbf6a7646736d0d7cf68c29a98
+D
+templates/index.html,f\6\f69b8ce2e31684ae06f1b3e84821e07e3dfddb2f
--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+import zoneinfo
+
+from flask import Flask, render_template
+
+import datetime
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template("index.html")
+
+
+@app.route('/heure')
+def heure():
+    date_local = datetime.datetime.now()
+    h = date_local.hour
+    m = date_local.minute
+    s = date_local.second
+    return render_template("heure.html", heure=h, minute=m, seconde=s)
 
 
 if __name__ == '__main__':
