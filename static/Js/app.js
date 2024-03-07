3038187a27a2173828639b6bf201718a84138ae5
LIBERT Quentin <quentinlibert59@gmail.com>
2024-03-07 11:51:57 +0100

    modification front de base.html

--- static/Js/app.js
+++ static/Js/app.js
@@ -1,8 +1,25 @@
-function showPopup(message) {
-  document.getElementById('popup-message').textContent = message;
-  document.getElementById('popup-container').style.display = 'block';
+function updateActiveNavItem() {
+  // Obtenez l'URL actuelle sans la partie query
+  var currentPath = window.location.pathname;
+
+  // Sélectionnez tous les éléments de navigation
+  var navItems = document.querySelectorAll('.nav .nav-item .nav-link');
+
+  // Boucle à travers les éléments de navigation
+  navItems.forEach(function(item) {
+    // Retirez la classe 'active' si elle est présente
+    item.classList.remove('active');
+
+    // Récupérez le chemin de l'élément de navigation
+    var itemPath = item.getAttribute('href');
+
+    // Si le chemin correspond à l'URL actuelle, ajoutez la classe 'active'
+    if (currentPath === itemPath) {
+      item.setAttribute('class','nav-link active')
+    }
+  });
 }
 
-function closePopup() {
-  document.getElementById('popup-container').style.display = 'none';
-}
+// Appeler la fonction au chargement de la page et à chaque changement d'état de l'historique
+document.addEventListener('DOMContentLoaded', updateActiveNavItem);
+window.addEventListener('popstate', updateActiveNavItem);
